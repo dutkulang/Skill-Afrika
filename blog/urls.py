48b944520970ab7b655d85a7ff4ffe2928c55981
Dut <dutandrew78@gmail.com>
2023-09-06 19:40:15 +0300

    added blog app

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('create-account/', views.create_account, name="create_account"),
+    path('login/', views.login, name="login"),
+    path("profiles/", views.profiles, name="profiles"),
+    path("profile/<int:pk>", views.profile, name="profile")
+]
